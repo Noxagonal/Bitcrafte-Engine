69f7a7afb9eab7e2c68811b6263cb2533af5ee59
Niko Kauppi <Noxagonal@gmail.com>
2023-12-18 16:29:43 -0500

    Add: BasicTimer.

---
+++ projects/engine/core/public/core/timer/BasicTimer.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <build_configuration/BuildConfigurationComponent.hpp>
+
+#include <chrono>
+
+
+
+namespace bc {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief
+/// Simple timer that can return passed time since last call.
+class BasicTimer
+{
+public:
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	inline									BasicTimer()
+	{
+		last_time_point	= std::chrono::steady_clock::now();
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// @brief
+	/// Tick once. Get amount of time passed since last call to this function.
+	///
+	///	If first call, then returns time since this timer was created.
+	/// 
+	/// @return
+	/// Amount of time passed in seconds.
+	inline double							Tick()
+	{
+		auto now		= std::chrono::steady_clock::now();
+		auto delta_time	= std::chrono::duration<double>( now - last_time_point ).count();
+		last_time_point	= now;
+		return delta_time;
+	}
+
+private:
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	std::chrono::steady_clock::time_point	last_time_point;
+};
+
+
+
+} // bc
