2008fe3cf9f077a4cc5b785f9d9651fb8079342c
Niko Kauppi <Noxagonal@gmail.com>
2023-12-21 18:32:06 -0500

    Refactor: Added missing info comment to Throw function and added BITCRAFTE_ENGINE_API to Exception class.

--- projects/engine/core/public/core/diagnostic/exception/Exception.hpp
+++ projects/engine/core/public/core/diagnostic/exception/Exception.hpp
@@ -17,7 +17,7 @@ namespace diagnostic {
 /// Primary exception used throughout the engine.
 ///
 ///	Catching this will catch all exceptions that the engine can natively throw.
-class Exception
+class BITCRAFTE_ENGINE_API Exception
 {
 public:
 
@@ -301,6 +301,40 @@ inline void												Throw [[noreturn]] (
 	);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief
+/// Throw exception with message and optional information.
+///
+/// Example:
+/// Eg. Index out of scope: Assuming list size is 5 and accessed index was 6.
+///		diagnostic::Throw(
+///			diagnostic::MakePrintRecord_AssertText( "Index out of scope",
+///				"List size", size,
+///				"Accessed index", index
+///			)
+///		);
+/// Will output:
+///		Index out of scope
+///		- List size: 5
+///		- Accessed index: 6
+///
+/// @note
+///	Depending on where the exception is thrown, this will either disable parts of the engine or cause general crash of the
+/// engine. For example if an Entity throws in its update logic, then the game is shut down and editor displays what caused the
+/// error. If the worker thread catches an exception directly, it'll cause the whole engine to shut down.
+///
+/// @warning
+/// All exceptions are disabled in final release build of the game, in which case call to this function crashes the
+/// application and lets the OS do the cleanup.
+///
+/// @see bc::diagnostic::PrintRecord
+///
+/// @param print_record
+///	bc::diagnostic::PrintRecord. General info of what caused the exception. Eg, "Index out of scope" when iterating over
+/// an array.
+///
+/// @param source_location
+/// Please leave this as default. This reports the source location where this function was called.
 template<container_bases::TextContainerCharacterType CharacterType, size_t StringArraySize>
 void													Throw [[noreturn]] (
 	const CharacterType( &message )[ StringArraySize ],
