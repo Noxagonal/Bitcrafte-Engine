897b1932d4803870479dc2155defce4e449118c3
Niko Kauppi <Noxagonal@gmail.com>
2023-12-28 01:45:41 -0500

    Add: Created vulkan swapchain implementation.

---
+++ projects/engine/rhi/rhi_vulkan/private/rhi_vulkan/vk/swapchain/VulkanSwapchainCreateInfo.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <build_configuration/BuildConfigurationComponent.hpp>
+
+
+
+namespace bc {
+namespace rhi {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct VulkanSwapchainCreateInfo
+{
+	bool												use_v_sync							= true;
+};
+
+
+
+} // rhi
+} // bc
