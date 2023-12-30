5341e41bbae2e2f9551095f43eeb6f4f558b0d8f
Niko Kauppi <Noxagonal@gmail.com>
2023-12-30 15:21:49 -0500

    Added basic synchronization primitives.

---
+++ projects/engine/rhi/rhi_vulkan/private/rhi_vulkan/vk/synchronization/VulkanFence.cpp
@@ -0,0 +1,61 @@
+
+#include <rhi_vulkan/PreCompiledHeader.hpp>
+#include <rhi_vulkan/vk/synchronization/VulkanFence.hpp>
+
+#include <rhi_vulkan/rhi_vulkan_impl/RHIVulkanImpl.hpp>
+#include <rhi_vulkan/vk/device/VulkanDevice.hpp>
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bc::rhi::VulkanFence::VulkanFence(
+	RHIVulkanImpl		&	rhi_vulkan_impl,
+	bool					initially_signaled
+) :
+	rhi_vulkan_impl( rhi_vulkan_impl )
+{
+	auto create_info = VkFenceCreateInfo {};
+	create_info.sType	= VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+	create_info.pNext	= nullptr;
+	create_info.flags	= initially_signaled ? VK_FENCE_CREATE_SIGNALED_BIT : 0;
+	BAssertVkResult( vkCreateFence(
+		rhi_vulkan_impl.GetVulkanDevice(),
+		&create_info,
+		rhi_vulkan_impl.GetMainThreadAllocationCallbacks(),
+		&vk_fence
+	) );
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bc::rhi::VulkanFence::~VulkanFence()
+{
+	Clear();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bc::rhi::VulkanFence & bc::rhi::VulkanFence::operator=(
+	VulkanFence && other
+) noexcept
+{
+	MoveOther( std::move( other ) );
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void bc::rhi::VulkanFence::Clear()
+{
+	if( vk_fence == VK_NULL_HANDLE ) return;
+	vkDestroyFence(
+		rhi_vulkan_impl.GetVulkanDevice(),
+		vk_fence,
+		rhi_vulkan_impl.GetMainThreadAllocationCallbacks()
+	);
+	vk_fence = VK_NULL_HANDLE;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void bc::rhi::VulkanFence::MoveOther(
+	VulkanFence && other
+)
+{
+	std::swap( vk_fence, other.vk_fence );
+}
