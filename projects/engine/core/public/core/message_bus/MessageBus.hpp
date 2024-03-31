3027c51db020d7ad087ea612335d6d6635054a77
Niko Kauppi <Noxagonal@gmail.com>
2024-03-30 23:41:54 -0400

    Added initial message bus implementation.

---
+++ projects/engine/core/public/core/message_bus/MessageBus.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <build_configuration/BuildConfigurationComponent.hpp>
+#include <core/message_bus/MessageBusMessage.hpp>
+#include <core/message_bus/MessageBusMessageHandler.hpp>
+#include <core/message_bus/MessageBusReceiver.hpp>
+#include <core/message_bus/MessageBusPacket.hpp>
+
+#include <core/utility/template/TypeList.hpp>
+#include <core/data_types/FundamentalTypes.hpp>
+#include <core/containers/Array.hpp>
+#include <core/containers/List.hpp>
+#include <core/containers/UniquePtr.hpp>
+
+#include <core/event/Event.hpp>
+
+#include <mutex>
+
+
+
+namespace bc {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief 
+/// Thread safe message bus which allows efficiently sending and receiving message packets.
+/// 
+/// Message bus is used to send and receive messages between systems. Messages are grouped into message packets, which makes
+/// sending and receiving messages more efficient. Each message packet can be created in isolation in one thread, sent to the
+/// bus, and received from the bus in another thread. The message bus can handle multiple types of message packets, but those
+/// message packet types must be given to the message bus as template arguments.
+/// 
+/// Each message packet is designed to contain multiple messages and for optimal performance, you should put as many messages
+/// into a single packet as possible, within reason. Once the packet is sent, it is owned by the bus until something else claims
+/// it. When a message packet is sent, an event is signalled to all listeners, the listener can then claim the packet by calling
+/// MessageBus::ClaimPacket(). After the packet has been claimed, its ownership is transferred to the receiver. The message
+/// packet will stay in the bus until it is claimed.
+/// 
+/// Data contained in messages is thread safe in a sense that no other thread, except the thread that accepted the message
+/// packet, is allowed to access the data. Pointers in messages however, are obviously not thread safe and should be used with
+/// care.
+/// 
+/// Message bus is meant for real-time data transfer, so it cannot be serialized or de-serialized.
+/// 
+/// @tparam ...MessageBusMessagePacketTypePack
+/// List of message packet types that are used to send and receive messages. Only these packet types are allowed to be sent and
+/// received via this message bus.
+template<typename ...MessageBusMessagePacketTypePack>
+class MessageBus
+{
+public:
+	using MessagePacketTypeList = utility::TypeList<MessageBusMessagePacketTypePack...>;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	template<typename MessageBusPacketType>
+	void SendPacket( UniquePtr<MessageBusPacketType> message_packet )
+	{
+		static_assert( std::is_base_of_v<MessageBusPacketBase, MessageBusPacketType>, "MessageBusPacketType must be derived from MessageBusPacketBase" );
+		static_assert( MessagePacketTypeList::template HasType<MessageBusPacketType>(), "MessageBusPacketType must have been introduced to the bus via template parameter pack argument, see MessageBus documentation" );
+
+		auto packet_id = [ this, &message_packet ]() -> u64
+			{
+				std::lock_guard lock( message_bus_mutex );
+
+				message_packet_id_counter++;
+				auto packet_id = message_packet_id_counter;
+
+				message_packet_list.PushBack( std::move( message_packet ) );
+				message_packet_type_index_list.PushBack( MessagePacketTypeList::template TypeToIndex<MessageBusPacketType>() );
+
+				message_packet_id_list.PushBack( packet_id );
+				return packet_id;
+			}( );
+
+		OnPacketSent.Signal( packet_id );
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	template<typename MessageBusPacketType>
+	[[nodiscard]]
+	UniquePtr<MessageBusPacketType> ClaimPacket( u64 packet_id )
+	{
+		static_assert( std::is_base_of_v<MessageBusPacketBase, MessageBusPacketType>, "MessageBusPacketType must be derived from MessagePacketBase" );
+		static_assert( MessagePacketTypeList::template HasType<MessageBusPacketType>(), "MessageBusPacketType must have been introduced to the bus via template parameter pack argument, see MessageBus documentation" );
+
+		std::lock_guard lock( message_bus_mutex );
+
+		auto packet_index = GetMessagePacketIndex( packet_id );
+		if( packet_index == std::numeric_limits<u64>::max() ) return nullptr; // Packet not found.
+		BAssert(
+			message_packet_type_index_list[ packet_index ] == MessagePacketTypeList::template TypeToIndex<MessageBusPacketType>(),
+			"Message bus packet found, but its type does not match, packet cannot be claimed until the correct type is given to "
+			"MessageBus::ClaimPacket<Type>(), packet will remain in the bus."
+		);
+		auto packet = std::move( message_packet_list[ packet_index ] );
+		ErasePacketSlot( packet_index );
+		return packet.CastTo<MessageBusPacketType>();
+	}
+
+private:
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	u64 GetMessagePacketIndex( u64 packet_id )
+	{
+		auto it = std::find( message_packet_id_list.begin(), message_packet_id_list.end(), packet_id );
+		if( it == message_packet_id_list.end() ) return std::numeric_limits<u64>::max();
+		return it.GetIndex();
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	void ErasePacketSlot( u64 packet_index )
+	{
+		message_packet_list.Erase( message_packet_list.begin() + packet_index );
+		message_packet_type_index_list.Erase( message_packet_type_index_list.begin() + packet_index );
+		message_packet_id_list.Erase( message_packet_id_list.begin() + packet_index );
+	}
+
+public:
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// @brief
+	/// Event that is signalled when a message packet is sent to the bus.
+	///
+	/// Packets can be claimed using MessageBus::ClaimPacket().
+	///
+	/// The event is signalled on the thread that sent the packet.
+	///
+	/// @see MessageBus::ClaimPacket()
+	///
+	/// @eventparam @ref u64
+	/// ID of the packet that was sent. Should be used to claim the packet.
+	Event<u64>									OnPacketSent;
+
+	// TODO: OnPacketReceived event must be made thread safe.
+
+private:
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	std::mutex									message_bus_mutex;
+
+	List<UniquePtr<MessageBusPacketBase>>		message_packet_list;
+	List<u64>									message_packet_type_index_list;
+	List<u64>									message_packet_id_list;
+
+	u64 										message_packet_id_counter				= 0;
+};
+
+
+
+} // bc
---
+++ tests/engine/core/source/message_bus/MessageBus.cpp
@@ -0,0 +1,192 @@
+
+#include <gtest/gtest.h>
+
+#include <core/CoreComponent.hpp>
+#include <core/message_bus/MessageBus.hpp>
+
+
+
+namespace core {
+
+
+
+static constexpr float		Basic_MAValue = 5.0f;
+static constexpr int		Basic_MBValue = 19;
+static constexpr double		Basic_MCValue = 1010.5;
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST( MessageBus, Basic )
+{
+	using namespace bc;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Sample message types
+	struct MessageA : public MessageBusMessage
+	{
+		MessageA( float value ) : value( value ) {}
+		float value = 0.0f;
+	};
+	struct MessageB : public MessageBusMessage
+	{
+		MessageB( int value ) : value( value ) {}
+		int value = 0;
+	};
+	struct MessageC : public MessageBusMessage
+	{
+		MessageC( double value ) : value( value ) {}
+		double value = 0.0;
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Sample message handlers
+	struct MessageHandlerA : public MessageBusMessageHandler<MessageA>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_FLOAT_EQ( message->value, Basic_MAValue );
+		}
+	};
+	struct MessageHandlerB : public MessageBusMessageHandler<MessageB>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_EQ( message->value, Basic_MBValue );
+		}
+	};
+	struct MessageHandlerC : public MessageBusMessageHandler<MessageC>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_DOUBLE_EQ( message->value, Basic_MCValue );
+		}
+	};
+
+	{
+		using Packet_1 = MessageBusPacket<MessageA, MessageB, MessageC>;
+		using Packet_2 = MessageBusPacket<MessageB, MessageC>;
+		using Packet_3 = MessageBusPacket<MessageA, MessageC>;
+		using Receiver_1 = MessageBusReceiver<MessageHandlerA, MessageHandlerB, MessageHandlerC>;
+		using Receiver_2 = MessageBusReceiver<MessageHandlerB, MessageHandlerC>;
+		using Receiver_3 = MessageBusReceiver<MessageHandlerA, MessageHandlerC>;
+
+		MessageBus<Packet_1, Packet_2, Packet_3> message_bus;
+
+		// Make packets.
+		UniquePtr<Packet_1> packet_1 = MakeUniquePtr<Packet_1>();
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		UniquePtr<Packet_2> packet_2 = MakeUniquePtr<Packet_2>();
+		packet_2->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
+		packet_2->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		UniquePtr<Packet_3> packet_3 = MakeUniquePtr<Packet_3>();
+		packet_3->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
+		packet_3->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+
+		// Send packet.
+		message_bus.SendPacket( std::move( packet_1 ) );
+		message_bus.SendPacket( std::move( packet_2 ) );
+		message_bus.SendPacket( std::move( packet_3 ) );
+
+		// Receive packet.
+		Receiver_1 receiver_1;
+		Receiver_2 receiver_2;
+		Receiver_3 receiver_3;
+		auto received_packet_1 = message_bus.ClaimPacket<Packet_1>( 1 );
+		auto received_packet_2 = message_bus.ClaimPacket<Packet_2>( 2 );
+		auto received_packet_3 = message_bus.ClaimPacket<Packet_3>( 3 );
+		receiver_1.ProcessMessages( received_packet_1.Get() );
+		receiver_2.ProcessMessages( received_packet_2.Get() );
+		receiver_3.ProcessMessages( received_packet_3.Get() );
+	}
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST( MessageBus, Event )
+{
+	using namespace bc;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Sample message types
+	struct MessageA : public MessageBusMessage
+	{
+		MessageA( float value ) : value( value ) {}
+		float value = 0.0f;
+	};
+	struct MessageB : public MessageBusMessage
+	{
+		MessageB( int value ) : value( value ) {}
+		int value = 0;
+	};
+	struct MessageC : public MessageBusMessage
+	{
+		MessageC( double value ) : value( value ) {}
+		double value = 0.0;
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Sample message handlers
+	struct MessageHandlerA : public MessageBusMessageHandler<MessageA>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_FLOAT_EQ( message->value, Basic_MAValue );
+		}
+	};
+	struct MessageHandlerB : public MessageBusMessageHandler<MessageB>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_EQ( message->value, Basic_MBValue );
+		}
+	};
+	struct MessageHandlerC : public MessageBusMessageHandler<MessageC>
+	{
+		virtual void operator()( const MessageType* message ) override
+		{
+			EXPECT_DOUBLE_EQ( message->value, Basic_MCValue );
+		}
+	};
+
+	{
+		using Packet_1 = MessageBusPacket<MessageA, MessageB, MessageC>;
+		using Receiver_1 = MessageBusReceiver<MessageHandlerA, MessageHandlerB, MessageHandlerC>;
+
+		auto message_bus		= MessageBus<Packet_1>();
+		auto receiver_1			= Receiver_1();
+
+		auto messages_handled	= false;
+
+		// Register callback.
+		message_bus.OnPacketSent.RegisterCallback( [ &message_bus, &receiver_1, &messages_handled ]( u64 packet_id )
+			{
+				EXPECT_EQ( packet_id, 1 );
+				auto packet_1 = message_bus.ClaimPacket<Packet_1>( packet_id );
+				packet_1->ReceiveMessages( receiver_1 );
+
+				messages_handled = true;
+			}
+		);
+
+		EXPECT_FALSE( messages_handled );
+
+		// Make packet.
+		auto packet_1 = bc::MakeUniquePtr<Packet_1>();
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
+		packet_1->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+
+		// Send packet.
+		message_bus.SendPacket( std::move( packet_1 ) );
+
+		EXPECT_TRUE( messages_handled );
+	}
+};
+
+
+
+} // core
