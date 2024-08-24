732db0bec1fdceb0f6e0bd85ddf63d6baf1c8458
Niko Kauppi <Noxagonal@gmail.com>
2024-08-24 16:41:05 -0400

    Fix: Fixed and updated tests for the new message bus message handling.

--- tests/engine/core/source/message_bus/MessageBus.cpp
+++ tests/engine/core/source/message_bus/MessageBus.cpp
@@ -18,17 +18,17 @@ static constexpr double		Basic_MCValue = 1010.5;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Sample message types
-struct MessageA : public bc::MessageBusMessage
+struct MessageA
 {
 	MessageA( float value ) : value( value ) {}
 	float value = 0.0f;
 };
-struct MessageB : public bc::MessageBusMessage
+struct MessageB
 {
 	MessageB( int value ) : value( value ) {}
 	int value = 0;
 };
-struct MessageC : public bc::MessageBusMessage
+struct MessageC
 {
 	MessageC( double value ) : value( value ) {}
 	double value = 0.0;
@@ -38,21 +38,23 @@ struct MessageC : public bc::MessageBusMessage
 // Sample message handlers
 struct MessageHandlerA : public bc::MessageBusMessageHandler<MessageA>
 {
-	virtual void operator()( const MessageType* message ) override
+	void operator()( const MessageType* message )
 	{
 		EXPECT_FLOAT_EQ( message->value, Basic_MAValue );
 	}
 };
 struct MessageHandlerB : public bc::MessageBusMessageHandler<MessageB>
 {
-	virtual void operator()( const MessageType* message ) override
+	MessageHandlerB() {};
+
+	void operator()( const MessageType* message )
 	{
 		EXPECT_EQ( message->value, Basic_MBValue );
 	}
 };
 struct MessageHandlerC : public bc::MessageBusMessageHandler<MessageC>
 {
-	virtual void operator()( const MessageType* message ) override
+	void operator()( const MessageType* message )
 	{
 		EXPECT_DOUBLE_EQ( message->value, Basic_MCValue );
 	}
@@ -77,15 +79,15 @@ TEST( MessageBus, Basic )
 
 		// Make packets.
 		UniquePtr<Packet_1> packet_1 = MakeUniquePtr<Packet_1>();
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_1->AddMessage( MessageA( Basic_MAValue ) );
+		packet_1->AddMessage( MessageB( Basic_MBValue ) );
+		packet_1->AddMessage( MessageC( Basic_MCValue ) );
 		UniquePtr<Packet_2> packet_2 = MakeUniquePtr<Packet_2>();
-		packet_2->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
-		packet_2->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_2->AddMessage( MessageB( Basic_MBValue ) );
+		packet_2->AddMessage( MessageC( Basic_MCValue ) );
 		UniquePtr<Packet_3> packet_3 = MakeUniquePtr<Packet_3>();
-		packet_3->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
-		packet_3->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_3->AddMessage( MessageA( Basic_MAValue ) );
+		packet_3->AddMessage( MessageC( Basic_MCValue ) );
 
 		// Send packet.
 		message_bus.SendPacket( std::move( packet_1 ) );
@@ -99,9 +101,9 @@ TEST( MessageBus, Basic )
 		auto received_packet_1 = message_bus.ClaimPacket<Packet_1>( 1 );
 		auto received_packet_2 = message_bus.ClaimPacket<Packet_2>( 2 );
 		auto received_packet_3 = message_bus.ClaimPacket<Packet_3>( 3 );
-		receiver_1.ProcessMessages( received_packet_1.Get() );
-		receiver_2.ProcessMessages( received_packet_2.Get() );
-		receiver_3.ProcessMessages( received_packet_3.Get() );
+		receiver_1.ProcessMessages( *received_packet_1 );
+		receiver_2.ProcessMessages( *received_packet_2 );
+		receiver_3.ProcessMessages( *received_packet_3 );
 	}
 };
 
@@ -126,7 +128,7 @@ TEST( MessageBus, Event )
 			{
 				EXPECT_EQ( packet_id, 1 );
 				auto packet_1 = message_bus.ClaimPacket<Packet_1>( packet_id );
-				packet_1->ReceiveMessages( receiver_1 );
+				receiver_1.ProcessMessages( *packet_1 );
 
 				messages_handled = true;
 			}
@@ -136,9 +138,9 @@ TEST( MessageBus, Event )
 
 		// Make packet.
 		auto packet_1 = bc::MakeUniquePtr<Packet_1>();
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_1->AddMessage( MessageA( Basic_MAValue ) );
+		packet_1->AddMessage( MessageB( Basic_MBValue ) );
+		packet_1->AddMessage( MessageC( Basic_MCValue ) );
 
 		// Send packet.
 		message_bus.SendPacket( std::move( packet_1 ) );
@@ -164,12 +166,12 @@ TEST( MessageBus, UnreceivablePacket )
 
 		// Make packets.
 		UniquePtr<Packet_1> packet_1 = MakeUniquePtr<Packet_1>();
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageA>( Basic_MAValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
-		packet_1->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_1->AddMessage( MessageA( Basic_MAValue ) );
+		packet_1->AddMessage( MessageB( Basic_MBValue ) );
+		packet_1->AddMessage( MessageC( Basic_MCValue ) );
 		UniquePtr<Packet_2> packet_2 = MakeUniquePtr<Packet_2>();
-		packet_2->AddMessage( bc::MakeUniquePtr<MessageB>( Basic_MBValue ) );
-		packet_2->AddMessage( bc::MakeUniquePtr<MessageC>( Basic_MCValue ) );
+		packet_2->AddMessage( MessageB( Basic_MBValue ) );
+		packet_2->AddMessage( MessageC( Basic_MCValue ) );
 
 		// Send packet.
 		message_bus.SendPacket( std::move( packet_1 ) );
@@ -186,6 +188,115 @@ TEST( MessageBus, UnreceivablePacket )
 	}
 };
 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bc::i64 MessageBus_EnsureDestructorCalled_handler_alive_count = 0;
+TEST( MessageBus, EnsureDestructorCalled )
+{
+	using namespace bc;
+
+	MessageBus_EnsureDestructorCalled_handler_alive_count = 0;
+
+	struct NonThrowingMessageHandler_A : public MessageBusMessageHandler<MessageA>
+	{
+		NonThrowingMessageHandler_A()
+		{
+			MessageBus_EnsureDestructorCalled_handler_alive_count++;
+		}
+		NonThrowingMessageHandler_A( const NonThrowingMessageHandler_A & other )
+		{
+			MessageBus_EnsureDestructorCalled_handler_alive_count++;
+		}
+		NonThrowingMessageHandler_A( NonThrowingMessageHandler_A && other )
+		{
+			MessageBus_EnsureDestructorCalled_handler_alive_count++;
+		}
+		~NonThrowingMessageHandler_A()
+		{
+			MessageBus_EnsureDestructorCalled_handler_alive_count--;
+		}
+		void operator()( const MessageType* message ) {}
+	};
+	struct NonThrowingMessageHandler_B : public NonThrowingMessageHandler_A {};
+	struct NonThrowingMessageHandler_C : public NonThrowingMessageHandler_A {};
+
+	{
+		using Receiver = MessageBusReceiver<NonThrowingMessageHandler_A, NonThrowingMessageHandler_B, NonThrowingMessageHandler_C>;
+
+		MessageBus_EnsureDestructorCalled_handler_alive_count = 0;
+
+		//auto message_bus		= MessageBus<Packet>();
+		Receiver();
+		EXPECT_EQ( MessageBus_EnsureDestructorCalled_handler_alive_count, 0 );
+	}
+	{
+		using Receiver = MessageBusReceiver<NonThrowingMessageHandler_C, NonThrowingMessageHandler_A, NonThrowingMessageHandler_B>;
+
+		MessageBus_EnsureDestructorCalled_handler_alive_count = 0;
+
+		//auto message_bus		= MessageBus<Packet>();
+		Receiver();
+		EXPECT_EQ( MessageBus_EnsureDestructorCalled_handler_alive_count, 0 );
+	}
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bc::i64 MessageBus_ThrowingHandlerConstructor_handler_alive_count = 0;
+TEST( MessageBus, ThrowingHandlerConstructor )
+{
+	using namespace bc;
+
+	struct NonThrowingMessageHandler_A : public MessageBusMessageHandler<MessageA>
+	{
+		NonThrowingMessageHandler_A()
+		{
+			MessageBus_ThrowingHandlerConstructor_handler_alive_count++;
+		}
+		NonThrowingMessageHandler_A( const NonThrowingMessageHandler_A & other )
+		{
+			MessageBus_ThrowingHandlerConstructor_handler_alive_count++;
+		}
+		NonThrowingMessageHandler_A( NonThrowingMessageHandler_A && other )
+		{
+			MessageBus_ThrowingHandlerConstructor_handler_alive_count++;
+		}
+		~NonThrowingMessageHandler_A()
+		{
+			MessageBus_ThrowingHandlerConstructor_handler_alive_count--;
+		}
+		void operator()( const MessageType* message ) {}
+	};
+	struct NonThrowingMessageHandler_B : public NonThrowingMessageHandler_A {};
+	
+	struct ThrowingMessageHandler : public NonThrowingMessageHandler_A
+	{
+		ThrowingMessageHandler() {
+			throw diagnostic::Exception( "ThrowingHandlerConstructor" );
+		}
+	};
+
+	{
+		using Receiver = MessageBusReceiver<ThrowingMessageHandler, NonThrowingMessageHandler_A, NonThrowingMessageHandler_B>;
+
+		MessageBus_ThrowingHandlerConstructor_handler_alive_count = 0;
+
+		//auto message_bus		= MessageBus<Packet>();
+		EXPECT_THROW( Receiver(), diagnostic::Exception );
+		EXPECT_EQ( MessageBus_ThrowingHandlerConstructor_handler_alive_count, 0 );
+	}
+	{
+		using Receiver = MessageBusReceiver<NonThrowingMessageHandler_A, NonThrowingMessageHandler_B, ThrowingMessageHandler>;
+
+		MessageBus_ThrowingHandlerConstructor_handler_alive_count = 0;
+
+		//auto message_bus		= MessageBus<Packet>();
+		EXPECT_THROW( Receiver(), diagnostic::Exception );
+		EXPECT_EQ( MessageBus_ThrowingHandlerConstructor_handler_alive_count, 0 );
+	}
+};
 
 
 } // core
